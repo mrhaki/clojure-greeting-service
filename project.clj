ea9a6adb8c31e999b87b026d8a04e4357c2a03a0
Hubert A. Klein Ikkink <h.kleinikkink@gmail.com>
2021-01-21 07:53:32 +0100

    Add implementation for greeting service

--- project.clj
+++ project.clj
@@ -2,9 +2,17 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+            :url "https://www.eclipse.org/legal/epl-2.0/"
+            }
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-core "1.8.2"]
+                 [ring/ring-json "0.5.0"]
+                 [compojure "1.6.2"]]
+  
   :main ^:skip-aot greeting-service.core
+  
   :target-path "target/%s"
+  
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- src/greeting_service/core.clj
+++ src/greeting_service/core.clj
@@ -1,7 +1,41 @@
 (ns greeting-service.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [ring.util.response :refer [response]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [compojure.core :refer [defroutes GET]])
   (:gen-class))
 
+(def counter (atom 0))
+
+(defn greeting-handler [name]
+  (let [message (format "Hello, %s!" (or name "World"))]
+    (swap! counter inc)
+    (response {:id @counter :content message})))
+
+(defroutes api
+           (GET "/greeting" [name] (greeting-handler name)))
+
+(def app (-> api
+             wrap-json-response
+             wrap-keyword-params
+             wrap-params))
+
+(defn start-server []
+  (run-server app {:port 8080}))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Start server to serve /greeting endpoint"
   [& args]
-  (println "Hello, World!"))
+  (start-server))
+
+
+(comment
+  ;; Stop server
+  (server)
+
+  ;; Start server
+  (def server (start-server))
+
+  )
