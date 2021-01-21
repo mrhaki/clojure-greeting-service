5a620334d6d82a430395147afa276ba9c9aa94aa
Hubert A. Klein Ikkink <h.kleinikkink@gmail.com>
2021-01-21 07:15:21 +0100

    Add initial project

---
+++ test/greeting_service/core_test.clj
@@ -0,0 +1,7 @@
+(ns greeting-service.core-test
+  (:require [clojure.test :refer :all]
+            [greeting-service.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
